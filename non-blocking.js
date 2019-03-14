e57fafc6380366a4c3c48a456984a89a66461078
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-03-13 23:14:15 -0500

    non blocking

---
+++ non-blocking.js
@@ -0,0 +1,17 @@
+function promedio1(nota1, nota2, nota3) {
+  let resultado = (nota1 + nota2 + nota3) / 3
+  console.log('El promedio 1 es ' + resultado);
+}
+
+let promedio2 = (nota1, nota2, nota3) => {
+  setTimeout(function () {
+    let resultado = (nota1 + nota2 + nota3) / 3
+    console.log('El promedio 2 es ' + resultado);
+  }, 2000)
+}
+
+let promedio3 = (nota1, nota2, nota3) => console.log('El promedio 3 es ' + (nota1 + nota2 + nota3) / 3);
+
+promedio1(3, 4, 5);
+promedio2(1, 2, 1);
+promedio3(5, 4, 5);
