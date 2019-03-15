0a0afb7682b45c8f0ecec4b46f98eaa4208357ef
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-03-15 18:15:48 -0500

    require native (UPDATED)

--- data-require.js
+++ data-require.js
@@ -1,4 +1,5 @@
 const { student, getAverage } = require('./calculations');
+const fs = require('fs');
 
 console.log("the student's name is " + student.name);
 console.log("the student's average is " + getAverage(student.notes.math, student.notes.english, student.notes.programation));
@@ -8,3 +9,15 @@ let { name, age: ageAlies, notes: { math, english, programation } } = student;
 console.log("the student's name is " + name);
 console.log("the student's average is " + getAverage(math, english, programation));
 console.log("the student's name is " + ageAlies);
+
+
+let createFile = (schoolChild) => {
+  testText = "the student's name is " + name + '\n' +
+    'has obtained an average of ' + getAverage(math, english, programation);
+  fs.writeFile('average.txt', testText, (err) => {
+    if (err) throw (err)
+    console.log('the file has been created');
+  })
+}
+
+createFile(student);
