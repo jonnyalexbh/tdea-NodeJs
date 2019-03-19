160f69c7cf16eacb3fc95d1ed57e992e2c1d481c
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-03-18 23:33:03 -0500

    delivery number 1

---
+++ app.js
@@ -0,0 +1,73 @@
+const { courses } = require('./courses');
+const fs = require('fs');
+
+const options = {
+  course_id: {
+    demand: true,
+    alias: 'i'
+  },
+  identification: {
+    demand: true,
+    alias: 'x'
+  },
+  name: {
+    demand: true,
+    alias: 'n'
+  }
+}
+
+/*
+ * list courses
+ * 
+ */
+let listCourses = () => {
+  Object.keys(courses).forEach(key => {
+    let value = courses[key]
+    setTimeout(() => {
+      console.log(`El curso se llama ${value.id}-${value.name} tiene una duración ${value.duration} y un valor de ${value.value}`);
+    }, 2000 * key);
+  })
+}
+
+/*
+ * create file
+ * 
+ */
+let createFile = (course, argv) => {
+  testText = `El estudiante ${argv.name} con cédula ${argv.identification} se ha matriculado en el curso ${course.id} - ${course.name} tiene una duración de ${course.duration} y un valor ${course.value}`;
+  fs.writeFile('inscripcion.txt', testText, (err) => {
+    if (err) throw (err)
+    console.log('se ha creado el archivo');
+  })
+}
+
+/*
+ * exist course
+ * 
+ */
+let existCourse = (course, argv) => {
+  if (course == undefined) {
+    console.log('Ha ingresado un ID que no corresponde a ningún curso');
+    listCourses();
+  }
+  else {
+    createFile(course, argv)
+  }
+}
+
+/*
+ * argv yargs
+ * 
+ */
+const argv = require('yargs')
+  .command('inscripcion', 'registrarse en educacion continua', options)
+  .argv
+
+if (argv._[0] == 'inscripcion') {
+  let course = courses.find(course => course.id == argv.course_id)
+  existCourse(course, argv)
+}
+else {
+  console.log('Educación Continua del Tecnológico de Antioquia');
+  listCourses();
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+Tecnológico de Antioquia
+=================
+### Primera entrega:
+
+``` bash
+# archivos
+app.js
+courses.js
+package.json
+readme.md
+```
+### Instalar las dependencias del proyecto:
+``` bash
+cd tdea-NodeJs/
+npm install
+```
+
+### Ejecutar proyecto:
+``` bash
+# listar cursos
+node app.js
+# inscribirme en un curso
+node app.js inscripcion
+node app.js inscripcion -i=2 -x=123 -n=Jonnyalex
+```
