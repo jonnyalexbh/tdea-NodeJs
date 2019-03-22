06787e22ee2c56e51250b74353a59e20031c1f91
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-03-22 08:32:46 -0500

    hbs partials

--- 02-workspace-express/src/app.js
+++ 02-workspace-express/src/app.js
@@ -5,13 +5,22 @@ const hbs = require('hbs');
 
 // static files
 const publicDirectory = path.join(__dirname, "../public");
+const partialsDirectory = path.join(__dirname, "../partials");
 app.use(express.static(publicDirectory));
+
+hbs.registerPartials(partialsDirectory);
 
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
   res.render('index', {
     student: 'Jonny Alexander'
+  });
+});
+
+app.get('/calculations', (req, res) => {
+  res.render('calculations', {
+    student: 'Tankis Alexis'
   });
 });
 
