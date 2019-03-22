04e7242038b1a7f9c9728dcb8ba0f1fca717b89d
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-03-22 08:10:34 -0500

    hbs module

--- 02-workspace-express/src/app.js
+++ 02-workspace-express/src/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const hbs = require('hbs');
 
 // static files
 const publicDirectory = path.join(__dirname, "../public");
 app.use(express.static(publicDirectory));
 
-console.log(publicDirectory);
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    student: 'Jonny Alexander'
+  });
+});
 
 app.listen(3000, () => {
   console.log('Escuchando por el puerto 3000');
