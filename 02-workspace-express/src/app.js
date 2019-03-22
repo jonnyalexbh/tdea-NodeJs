498f10d77f0df08871ed3972c4d9ed015f4d46b0
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-03-22 07:51:03 -0500

    home workspace express

---
+++ 02-workspace-express/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// static files
+const publicDirectory = path.join(__dirname, "../public");
+app.use(express.static(publicDirectory));
+
+console.log(publicDirectory);
+
+app.listen(3000, () => {
+  console.log('Escuchando por el puerto 3000');
+});
