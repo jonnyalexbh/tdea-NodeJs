707ddbb8f2ae71c5d8cd2597de9ee166c6583fa7
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-03-23 14:10:08 -0500

    install hbs

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const hbs = require('hbs');
 
 // static files
 const publicDirectory = path.join(__dirname, "../public");
 app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(3000, () => {
   console.log('Escuchando por el puerto 3000');
