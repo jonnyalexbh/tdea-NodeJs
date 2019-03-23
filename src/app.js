96eb65ab4bf81fa04e4435770ac9083a8c868e9b
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-03-23 14:35:44 -0500

    install bootstrap 4.3.1

--- src/app.js
+++ src/app.js
@@ -6,7 +6,15 @@ const hbs = require('hbs');
 
 // static files
 const publicDirectory = path.join(__dirname, "../public");
+const dirNode_modules = path.join(__dirname , '../node_modules')
+
 app.use(express.static(publicDirectory));
+
+// install boostrap
+app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
+app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
+app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
+app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
 
 app.set('view engine', 'hbs');
 
