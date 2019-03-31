8ade06146b4361f29165a1f529bc7a5cc62002ba
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-03-31 10:56:30 -0500

    borrar pero antes subir

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 
 const express = require('express');
+
 const app = express();
 const path = require('path');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
 
-routes = require('../app/routes')
+const routes = require('../app/routes');
 
 // static files
-const publicDirectory = path.join(__dirname, "../public");
-const dirNode_modules = path.join(__dirname, '../node_modules')
-const partialsDirectory = path.join(__dirname, "../partials");
+const publicDirectory = path.join(__dirname, '../public');
+const nodeModulesDir = path.join(__dirname, '../node_modules');
+const partialsDirectory = path.join(__dirname, '../partials');
 
 app.use(express.static(publicDirectory));
 hbs.registerPartials(partialsDirectory);
@@ -20,10 +21,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
 
 // install boostrap
-app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
-app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
-app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
-app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
+app.use('/css', express.static(path.join(nodeModulesDir, '/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(nodeModulesDir, '/jquery/dist')));
+app.use('/js', express.static(path.join(nodeModulesDir, '/popper.js/dist')));
+app.use('/js', express.static(path.join(nodeModulesDir, '//bootstrap/dist/js')));
 
 app.set('view engine', 'hbs');
 
