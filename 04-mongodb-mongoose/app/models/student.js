fceda68e5d5da383d6bf9fe1d8f71f7ed8032a05
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-04-08 20:51:39 -0500

    validate the scheme

--- 04-mongodb-mongoose/app/models/student.js
+++ 04-mongodb-mongoose/app/models/student.js
@@ -1,5 +1,6 @@
 
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const Schema = mongoose.Schema;
 
@@ -7,17 +8,30 @@ const StudentSchema = new Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
+    unique: true,
   },
   math: {
     type: Number,
+    default: 0,
+    min: 0,
+    max: [5, 'enter a number in math'],
   },
   english: {
     type: Number,
+    default: 0,
+    min: 0,
+    max: 5,
   },
   programming: {
     type: Number,
+    default: 0,
+    min: 0,
+    max: 5,
   }
 });
+
+StudentSchema.plugin(uniqueValidator);
 
 const Student = mongoose.model('Student', StudentSchema);
 
