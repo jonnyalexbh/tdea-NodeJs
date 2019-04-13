79448d084fed3a47ab7089c88309b34afd585710
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-04-12 22:39:24 -0500

    writing your first route

--- 05-codecademy/app.js
+++ 05-codecademy/app.js
@@ -3,6 +3,14 @@ const app = express();              // Instantiate the app here
 
 const PORT = process.env.PORT || 4001;
 
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
+// Open a call to `app.get()` below:
+app.get('/expressions', (req, res, next) => {
+  res.send('Hello World!');
+});
+
 // Invoke the app's `.listen()` method below:
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
