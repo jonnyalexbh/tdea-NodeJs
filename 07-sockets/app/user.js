a0eafe8845b37a78af3a38bc368e4af42897a37f
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-04-20 15:09:12 -0500

    user model sockets

---
+++ 07-sockets/app/user.js
@@ -0,0 +1,31 @@
+class User {
+
+  constructor() {
+    this.users = [];
+  }
+
+  addUser(id, name) {
+    let user = { id, name }
+    this.users.push(user)
+    return this.users;
+  }
+
+  getUsers() {
+    return this.users
+  }
+
+  getUser(id) {
+    let user = this.users.filter(user => user.id === id)[0]
+    return user
+  }
+
+  removeUser(id) {
+    let userRemove = this.getUser(id)
+    this.users = this.users.filter(user => user.id != id)
+    return userRemove
+  }
+}
+
+module.exports = {
+  User
+}
