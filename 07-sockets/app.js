953864231266bb236d0fd96b8e64fa110d859d5d
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-04-20 13:01:37 -0500

    introduction to sockets

--- 07-sockets/app.js
+++ 07-sockets/app.js
@@ -2,13 +2,20 @@ const express = require('express'); // Import the express library here
 const path = require('path');
 const app = express();              // Instantiate the app here
 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
 require('./app/config');
 
 // static files
 const publicDirectory = path.join(__dirname, 'public');
 app.use(express.static(publicDirectory));
 
+io.on('connection', client => {
+  console.log('a user has connected');
+});
+
 // Invoke the app's `.listen()` method below:
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);
 });
